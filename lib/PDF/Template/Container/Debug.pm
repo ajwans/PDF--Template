4cf6736afd432cb7c2230382ff04b7ee878f35a2
Andrew Wansink <andy@halogix.com>
2011-04-19 01:20:15 +1000

    Created <debug> element
    Use the <debug> element when you want information about
    what enclosed elements are being put to the PDF.

---
+++ lib/PDF/Template/Container/Debug.pm
@@ -0,0 +1,91 @@
+package PDF::Template::Container::Debug;
+
+use strict;
+
+BEGIN {
+    use vars qw(@ISA);
+    @ISA = qw(PDF::Template::Container);
+
+    use PDF::Template::Container;
+}
+
+sub new
+{
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+
+    return $self;
+}
+
+sub enter_scope
+{
+    my $self = shift;
+    my ($context) = @_;
+
+	$context->{DEBUG}++;
+
+    $self->SUPER::enter_scope($context);
+
+	return 1;
+}
+
+sub exit_scope
+{
+    my $self = shift;
+    my ($context) = @_;
+
+	$context->{DEBUG}--;
+
+    return $self->SUPER::exit_scope($context);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+PDF::Template::Container::Debug
+
+=head1 PURPOSE
+
+Output debugging information when placing the enclosed elements
+
+=head1 NODE NAME
+
+DEBUG
+
+=head1 INHERITANCE
+
+PDF::Template::Container
+
+=head1 ATTRIBUTES
+
+None
+
+=head1 CHILDREN
+
+None
+
+=head1 AFFECTS
+
+STDOUT
+
+=head1 DEPENDENCIES
+
+None
+
+=head1 USAGE
+
+  <debug>...</debug>
+
+That produces debugging output for the elements between
+the debug tags.
+
+=head1 AUTHOR
+
+Andrew Wansink (andy@halogix.com)
+
+=head1 SEE ALSO
+
+=cut
